95dff6dded1c6a9e1b68e9c8458cc6711e1ef31b
Kristopher Romero <kristo0120@gmail.com>
2023-10-20 17:14:20 -0500

    Final entrega

--- main/main.c
+++ main/main.c
@@ -7,15 +7,33 @@ int conteo_ciclos = 0;
 void app_main()
 
 {
-    conteo_ciclos++;
-
     int otaUpdate = 0;
+
+    conteo_ciclos++;
+     switch (conteo_ciclos) {
+    case 1:
+        twai_main(); // Modulo de comunicacion CAN
+      break;
+    case 2:
+       i2c_main(); // Modulo de comunicacion i2c
+      break;
+    case 3:
+        twai_rectificadores_main();
+      break;
+    case 4:
+        if(otaUpdate) {
+         ota_main();
+        }
+      break;
+    default:
+      // if nothing else matches, do the default
+      // default is optional
+      break;
+  }
+
+
     // get a plataforma trayendo un bool update = get result
-    if(otaUpdate) {
-        ota_main();
-    }
-    i2c_main(); // Modulo de comunicacion i2c
-    twai_main(); // Modulo de comunicacion CAN
-    twai_rectificadores_main();
+    
+    
 
 }
